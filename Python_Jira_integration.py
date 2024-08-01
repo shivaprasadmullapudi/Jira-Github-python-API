6481a2978d0acde9a461e1160fb6eccfe671d1e9
shivaprasadmullapudi <106911000+shivaprasadmullapudi@users.noreply.github.com>
2024-08-01 12:23:27 -0400

    Update Python_Jira_integration.py

--- Python_Jira_integration.py
+++ Python_Jira_integration.py
@@ -6,7 +6,7 @@ import json
 
 url = "https://shivaprasadmullapudi8.atlassian.net/rest/api/3/issue"
 
-auth = HTTPBasicAuth("shivaprasadmullapudi8@gmail.com",)
+auth = HTTPBasicAuth("shivaprasadmullapudi8@gmail.com","API_TOKEN")
 
 headers = {
   "Accept": "application/json",
